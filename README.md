e78912e560575754bd0b22e97334818d6434f5bc
yh-habosol <k0912h@naver.com>
2024-02-12 16:10:34 +0900

    like button, join button

--- README.md
+++ README.md
@@ -1,16 +1,54 @@
-# practice
+To Do List
 
-A new Flutter project.
 
-## Getting Started
+회원가입 하면 해당 유저 정보 Users에 저장 (v)
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+로그인하면 -> 해당 유저 정보 Users에서 가져옴 (v)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+
+
+게시판 페이지로 이동 -> 해당 페이지에서는 Posts에서 post를 
+		        작성 일자 순으로 나열(제목만), 각각은 버튼 (v)
+
+
+
+각 게시판 게시글 누르면 -> 게시글 세부 페이지로 이동, 글 내용, 댓글 보여야 함 (v)
+
+
+
+게시글 작성 버튼 누르면 -> title, content, maxNumber 입력하고 생성 버튼 누르기 (v)
+
+
+
+게시글 -> 로그인 한 유저의 게시글일 경우만 edit, delete 표시 보이기 (v)
+	 게시글 올리면 해당 유저의 posts 컬렉션에 해당 post 추가해야 함 (v)
+	 Posts 컬렉션에 해당 post 추가 (v)
+
+
+
+
+///게시글 CRUD 과제
+- 수정이나 삭제 후 새로고침 해야 반영되는 문제
+- location 정보 가져와서 저정도 해야함
+- 좋아요 버튼 누르면 숫자 올라가고 해당 post에 반영 (v)
+- join 버튼 누르면 -> 정원 다 차기 전이면 추가되고 참여 인원 수 증가 (v)
+- join 누르면 -> user join_challenge 컬렉션에 해당 post 하나 추가
+- join 버튼 누르면 해당 post에 join_users 컬랙션에 user 추가
+- delete 버튼 누르면 -> post에 join_users에 있는 각 user에 대해 join_challenge에서 해당 post
+		      삭제
+- 댓글
+    -댓글 달기
+    -댓글 단 유저와 로그인한 유저 같으면 edit, delete버튼 보이게 하기
+
+
+
+
+
+
+지도 페이지 -> 현재 내 위치 좌표 기준 반경 몇 미터 이내 좌표 게시물 목록 나열 ()
+
+
+
+회원정보 수정 -> profile image 설정 ()
--- lib/post_detail.dart
+++ lib/post_detail.dart
@@ -1,11 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
 
-class PostDetailPage extends StatelessWidget {
+class PostDetailPage extends StatefulWidget {
   final String postId;
   const PostDetailPage({Key? key, required this.postId}) : super(key: key);
+
+  @override
+  _PostDetailPageState createState() => _PostDetailPageState();
+}
+
+class _PostDetailPageState extends State<PostDetailPage> {
+  late int numLike;
+  late bool isLiked;
+  late int numParticipate;
+  late int maxNumber;
+  late bool isParticipating;
+  late bool isCurrentUserAuthor;
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +26,10 @@ class PostDetailPage extends StatelessWidget {
         title: const Text("Community Detail"),
       ),
       body: FutureBuilder<DocumentSnapshot>(
-        future:
-            FirebaseFirestore.instance.collection('Posts').doc(postId).get(),
+        future: FirebaseFirestore.instance
+            .collection('Posts')
+            .doc(widget.postId)
+            .get(),
         builder:
             (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -28,7 +42,6 @@ class PostDetailPage extends StatelessWidget {
             return const Text('Post not found');
           }
 
-          // postId에 해당하는 문서 데이터
           final Map<String, dynamic> data =
               snapshot.data!.data() as Map<String, dynamic>;
 
@@ -37,12 +50,20 @@ class PostDetailPage extends StatelessWidget {
           final String content = data['content'] ?? "N/A";
           final DateTime createdAt =
               DateTime.fromMillisecondsSinceEpoch(data['createdAt']);
-          final int numParticipate = data['numParticipate'] ?? -1;
-          final int maxNumber = data['maxNumber'] ?? -1;
-          final int numLike = data['numLike'] ?? -1;
+
+          // 변경: 참여 상태 및 수를 Firestore에서 가져옴
+          maxNumber = data['maxNumber'] ?? -1;
+          numParticipate = data['numParticipate'] ?? 0;
+          isParticipating = (data['participants'] ?? [])
+              .contains(FirebaseAuth.instance.currentUser?.uid);
+
+          // 변경: 좋아요 상태 및 수를 Firestore에서 가져옴
+          numLike = data['numLike'] ?? 0;
+          isLiked = (data['likedBy'] ?? [])
+              .contains(FirebaseAuth.instance.currentUser?.uid);
 
           final String? currentUserId = FirebaseAuth.instance.currentUser?.uid;
-          bool isCurrentUserAuthor = currentUserId == data['userId'];
+          isCurrentUserAuthor = currentUserId == data['userId'];
 
           return Center(
             child: Column(
@@ -51,10 +72,34 @@ class PostDetailPage extends StatelessWidget {
                 Text("author: $author"),
                 Text("title: $title"),
                 Text("content: $content"),
-                Text("$numParticipate / $maxNumber"),
+                Text("Current Participants: $numParticipate / $maxNumber"),
                 Text(
                     "CreatedAt: ${createdAt.year} ${createdAt.month} ${createdAt.day}"),
-                Text("numLike: $numLike"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // 변경: 참여 가능한 버튼
+                    ElevatedButton(
+                      onPressed: () {
+                        // 변경: 참여 버튼을 누를 때의 동작
+                        handleJoinButton();
+                      },
+                      child: Text(isParticipating ? 'Disjoin' : 'Join'),
+                    ),
+                    // 변경: 좋아요 기능 추가
+                    IconButton(
+                      icon: Icon(
+                          isLiked ? Icons.favorite : Icons.favorite_border),
+                      color: isLiked ? Colors.red : null,
+                      onPressed: () {
+                        // 변경: 토글 버튼을 누를 때의 동작
+                        handleLikeButton();
+                      },
+                    ),
+                    // 변경: 좋아요 수 표시
+                    Text("Likes: $numLike"),
+                  ],
+                ),
                 if (isCurrentUserAuthor)
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -63,7 +108,7 @@ class PostDetailPage extends StatelessWidget {
                         onPressed: () {
                           // Edit 버튼 누를 때의 동작
                           Routemaster.of(context)
-                              .push('/community/$postId/edit');
+                              .push('/community/${widget.postId}/edit');
                         },
                         child: const Text('Edit'),
                       ),
@@ -72,7 +117,7 @@ class PostDetailPage extends StatelessWidget {
                         onPressed: () {
                           // Delete 버튼 누를 때의 동작
                           Routemaster.of(context)
-                              .push('/community/$postId/delete');
+                              .push('/community/${widget.postId}/delete');
                         },
                         child: const Text('Delete'),
                       ),
@@ -84,5 +129,127 @@ class PostDetailPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void handleJoinButton() async {
+    final String? currentUserId = FirebaseAuth.instance.currentUser?.uid;
+
+    if (isCurrentUserAuthor) {
+      // 변경: 만약 현재 사용자가 글 작성자라면, 해당 팝업 표시
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('You are the Creator'),
+            content: const Text('You are the creator of this post.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    if (isParticipating) {
+      numParticipate--;
+      await FirebaseFirestore.instance
+          .collection('Posts')
+          .doc(widget.postId)
+          .update({
+        'numParticipate': numParticipate,
+        'participants':
+            FieldValue.arrayRemove([FirebaseAuth.instance.currentUser?.uid]),
+      });
+
+      // 변경: 사용자의 join_challenges 컬렉션에서 post ID 제거
+      await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(currentUserId)
+          .collection('join_challenges')
+          .doc(widget.postId)
+          .delete();
+    } else {
+      if (numParticipate < maxNumber) {
+        numParticipate++;
+        await FirebaseFirestore.instance
+            .collection('Posts')
+            .doc(widget.postId)
+            .update({
+          'numParticipate': numParticipate,
+          'participants':
+              FieldValue.arrayUnion([FirebaseAuth.instance.currentUser?.uid]),
+        });
+
+        // 변경: 사용자의 join_challenges 컬렉션에 post ID 추가
+        await FirebaseFirestore.instance
+            .collection('Users')
+            .doc(currentUserId)
+            .collection('join_challenges')
+            .doc(widget.postId)
+            .set({});
+      } else {
+        // 변경: 인원이 다 찼을 때 팝업 표시
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Maximum Participants Reached'),
+              content: const Text(
+                  'The maximum number of participants has been reached.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+        return; // 변경: 인원이 다 찼을 때 더 이상 코드 진행하지 않도록 리턴
+      }
+    }
+
+    // 변경: 참여 상태 업데이트
+    setState(() {
+      isParticipating = !isParticipating;
+    });
+  }
+
+  void handleLikeButton() async {
+    // 변경: 토글하고 좋아요 상태 및 수 업데이트
+    if (isLiked) {
+      numLike--;
+      await FirebaseFirestore.instance
+          .collection('Posts')
+          .doc(widget.postId)
+          .update({
+        'numLike': numLike,
+        'likedBy':
+            FieldValue.arrayRemove([FirebaseAuth.instance.currentUser?.uid]),
+      });
+    } else {
+      numLike++;
+      await FirebaseFirestore.instance
+          .collection('Posts')
+          .doc(widget.postId)
+          .update({
+        'numLike': numLike,
+        'likedBy':
+            FieldValue.arrayUnion([FirebaseAuth.instance.currentUser?.uid]),
+      });
+    }
+
+    // 변경: 좋아요 상태 업데이트
+    setState(() {
+      isLiked = !isLiked;
+    });
   }
 }
